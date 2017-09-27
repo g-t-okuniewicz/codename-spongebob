04005aacb8b9370214964c49c3a4532e66648025
Adem Slavotic <SlavoticA@Adems-MacBook-Pro.local>
2017-09-27 17:55:12 +0100

    Energy network battery

---
+++ Assets/Components/C3_engineering_energy/Energy_script/EnergyNetwork.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class EnergyNetwork : MonoBehaviour 
+{
+	private float batteryFull;
+	private int batteryDuration;
+
+	//-----------------------------
+	public int GetTotalSeconds()
+	{
+		return batteryDuration;
+	}
+
+	//-----------------------------
+	public void Resetbattery(int seconds)
+	{
+		batteryFull = Time.time;
+		batteryDuration = seconds;
+	}
+
+	//-----------------------------
+	public int GetSecondsRemaining()
+	{
+		int elapsedSeconds = (int)(Time.time - batteryFull);
+		int secondsLeft = (BatteryDuration - elapsedSeconds);
+		return secondsLeft;
+	}
+
+	//-----------------------------
+	public float GetProportionTimeRemaining()
+	{
+		float proportionLeft = (float)GetSecondsRemaining() / (float)GetTotalSeconds();
+		return proportionLeft;
+	}
+}
